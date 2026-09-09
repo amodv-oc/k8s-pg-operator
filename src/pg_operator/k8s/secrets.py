e31695f064bf5d340f8c8d95173dc90e03073547
Amod Vithanage <amodv@ourcommunity.com.au>
2026-09-09 15:32:12 +1000

    Add spring_datasource_url

--- src/pg_operator/k8s/secrets.py
+++ src/pg_operator/k8s/secrets.py
@@ -209,4 +209,7 @@ def connection_keys(
             f"jdbc:postgresql://{host}:{port}/{database}"
             f"?user={safe_user}&password={safe_password}&sslmode={ssl_mode}"
         )
+        data["spring_datasource_url"] = (
+            f"jdbc:postgresql://{host}:{port}/{database}"
+        )
     return data
